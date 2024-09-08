9cc9b66e964fb551c1492a4b18f2a865feaaa490
AbdulManan Ali <abdulmananali46@gmail.com>
2024-09-09 01:32:13 +0500

    new commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,84 @@
-import AdminSidebar from "../components/AdminSidebar"
-
+import AdminSidebar from "../components/AdminSidebar";
+import { BsSearch } from "react-icons/bs";
+import { HiTrendingUp, HiTrendingDown } from "react-icons/hi";
 
 const Dashboard = () => {
   return (
     <div className="admin-container">
-        <AdminSidebar />
+      <AdminSidebar />
+
+      <main className="dashboard">
+        <div className="bar">
+          <BsSearch />
+          <input type="text" placeholder="Search for data, users, docs" />
+        </div>
+
+        {/* {widget} */}
+        <section className="widget-container">
+          <WidgetItem
+            heading="Revenue"
+            value={4777}
+            percent={38}
+            amount={true}
+            color="rgb(0,115,255)"
+          />
+          <WidgetItem
+            heading="Revenue"
+            value={4777}
+            percent={38}
+            amount={true}
+            color="rgb(0,115,255)"
+          />
+          <WidgetItem
+            heading="Revenue"
+            value={4777}
+            percent={38}
+            amount={true}
+            color="rgb(0,115,255)"
+          />
+          <WidgetItem
+            heading="Revenue"
+            value={4777}
+            percent={38}
+            amount={true}
+            color="rgb(0,115,255)"
+          />
+        </section>
+      </main>
     </div>
-  )
+  );
+};
+
+interface WidgetItemProps {
+  heading: string;
+  value: number;
+  percent: number;
+  amount?: boolean;
+  color: string;
 }
 
-export default Dashboard
+const WidgetItem = ({
+  heading,
+  value,
+  percent,
+  amount,
+  color,
+}: WidgetItemProps) => (
+  <article className="widget">
+    <div className="widgetInfo">
+      <p>{heading}</p>
+      {amount ? `$${value}` : value}
+      {percent > 0 ? (
+        <span className="green">
+          <HiTrendingUp /> {percent}%{" "}
+        </span>
+      ) : (
+        <span className="red">
+          <HiTrendingDown /> {percent}%{" "}
+        </span>
+      )}
+    </div>
+  </article>
+);
+
+export default Dashboard;
