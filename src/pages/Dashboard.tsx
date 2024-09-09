a268d742f73944eebe458b0f2caad768bd2cf479
AbdulManan Ali <abdulmananali46@gmail.com>
2024-09-10 01:15:16 +0500

    new commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,8 @@
 import AdminSidebar from "../components/AdminSidebar";
 import { BsSearch } from "react-icons/bs";
 import { HiTrendingUp, HiTrendingDown } from "react-icons/hi";
+import { FaRegBell } from "react-icons/fa";
+import userImage from  "../assets/userpic.png"
 
 const Dashboard = () => {
   return (
@@ -11,6 +13,8 @@ const Dashboard = () => {
         <div className="bar">
           <BsSearch />
           <input type="text" placeholder="Search for data, users, docs" />
+          <FaRegBell />
+          <img src={userImage} alt="" />
         </div>
 
         {/* {widget} */}
@@ -18,7 +22,7 @@ const Dashboard = () => {
           <WidgetItem
             heading="Revenue"
             value={4777}
-            percent={38}
+            percent={-9}
             amount={true}
             color="rgb(0,115,255)"
           />
@@ -38,7 +42,7 @@ const Dashboard = () => {
           />
           <WidgetItem
             heading="Revenue"
-            value={4777}
+            value={-2}
             percent={38}
             amount={true}
             color="rgb(0,115,255)"
@@ -67,6 +71,7 @@ const WidgetItem = ({
   <article className="widget">
     <div className="widgetInfo">
       <p>{heading}</p>
+
       {amount ? `$${value}` : value}
       {percent > 0 ? (
         <span className="green">
@@ -77,6 +82,24 @@ const WidgetItem = ({
           <HiTrendingDown /> {percent}%{" "}
         </span>
       )}
+    </div>
+
+    <div
+      className="widget-circle"
+      style={{
+        background: `conic-gradient(
+        ${color} ${(Math.abs(percent) / 100) * 360}deg,
+        rgb(255, 255, 255) 0
+      )`,
+      }}
+    >
+      <span
+        style={{
+          color,
+        }}
+      >
+        {percent}%
+      </span>
     </div>
   </article>
 );
